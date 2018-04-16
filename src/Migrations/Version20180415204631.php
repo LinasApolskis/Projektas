1fcdde71d5b49be427c4963fadf3086002398ceb
Paulius <dzenkaitis.paulius@gmail.com>
2018-04-16 19:39:35 +0300

    added captcha, improvet visibility for logged and not logged person

--- src/Migrations/Version20180415204631.php
+++ src/Migrations/Version20180415204631.php
@@ -8,7 +8,7 @@ use Doctrine\DBAL\Schema\Schema;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-class Version20180321202303 extends AbstractMigration
+class Version20180415204631 extends AbstractMigration
 {
     public function up(Schema $schema)
     {
@@ -16,6 +16,9 @@ class Version20180321202303 extends AbstractMigration
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('CREATE TABLE car (id INT AUTO_INCREMENT NOT NULL, username VARCHAR(25) NOT NULL, licencenumber VARCHAR(25) NOT NULL, brand VARCHAR(25) NOT NULL, model VARCHAR(25) NOT NULL, year INT NOT NULL, body VARCHAR(25) NOT NULL, fuel VARCHAR(25) NOT NULL, engine VARCHAR(25) NOT NULL, power INT NOT NULL, gearbox VARCHAR(25) NOT NULL, mileage INT NOT NULL, UNIQUE INDEX UNIQ_773DE69DE4276C68 (licencenumber), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, username VARCHAR(25) NOT NULL, password VARCHAR(64) NOT NULL, email VARCHAR(254) NOT NULL, is_active TINYINT(1) NOT NULL, UNIQUE INDEX UNIQ_8D93D649F85E0677 (username), UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE user_profile (username VARCHAR(25) NOT NULL, city VARCHAR(25) NOT NULL, address VARCHAR(25) NOT NULL, phone VARCHAR(25) NOT NULL, name VARCHAR(25) NOT NULL, surname VARCHAR(25) NOT NULL, PRIMARY KEY(username)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE visit (id INT AUTO_INCREMENT NOT NULL, username VARCHAR(25) NOT NULL, licencenumber VARCHAR(25) NOT NULL, date DATETIME NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
     }
 
     public function down(Schema $schema)
@@ -24,5 +27,8 @@ class Version20180321202303 extends AbstractMigration
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('DROP TABLE car');
+        $this->addSql('DROP TABLE user');
+        $this->addSql('DROP TABLE user_profile');
+        $this->addSql('DROP TABLE visit');
     }
 }
